a93a72855730e38bc327b2e606dc09d2993ac062
Tim Loden <tim.m.loden@gmail.com>
2019-03-07 12:10:26 -0800

    added ssl redirect to htaccess

--- bash-scripts/azure-wp-cli-create-multisite.sh
+++ bash-scripts/azure-wp-cli-create-multisite.sh
@@ -40,9 +40,10 @@ RewriteRule ^ - [L]
 RewriteRule ^([_0-9a-zA-Z-]+/)?(wp-(content|admin|includes).*) \$2 [L]
 RewriteRule ^([_0-9a-zA-Z-]+/)?(.*\.php)$ \$2 [L]
 RewriteRule . index.php [L]
-EOL
 
-#cat .htaccess ;
+RewriteCond %{HTTP:X-ARR-SSL} ^$
+RewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
+EOL
 
 #Install aiom
 wp plugin install all-in-one-wp-migration https://servmask.com/purchase/f2de6bb7-d3b8-442c-b40d-07a25856f822 https://servmask.com/purchase/49fb4af3-acaf-4e63-91a5-ca3df9f67ab7 --activate ;
@@ -50,15 +51,16 @@ wp plugin install all-in-one-wp-migration https://servmask.com/purchase/f2de6bb7
 #Update plugins just in case
 wp plugin update --all ;
 
-
-echo "*****************************************************************************************" ;
-<<EOL
+__odi="
    __________  ______   ____  ____  ____   _       __               ______                     
   / ____/ __ \/_  __/  / __ \/ __ \/  _/  | |     / /___  _________/ / __ \________  __________
  / /   / / / / / /    / / / / / / // /    | | /| / / __ \/ ___/ __  / /_/ / ___/ _ \/ ___/ ___/
 / /___/ /_/ / / /    / /_/ / /_/ // /     | |/ |/ / /_/ / /  / /_/ / ____/ /  /  __(__  |__  ) 
 \____/_____/ /_/     \____/_____/___/     |__/|__/\____/_/   \__,_/_/   /_/   \___/____/____/  
                                                                                                
-EOL
+"
+
+echo "*****************************************************************************************" ;
+echo "$__odi"
 echo "Install completed, you can now go to: $5" ;
 echo "*****************************************************************************************" ;
