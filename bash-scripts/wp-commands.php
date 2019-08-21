ef43fbdcef4cdbcd435b02cf626105cf21aa39fc
Tim Loden <tim.m.loden@gmail.com>
2019-08-21 14:58:04 -0700

    one line for test

--- bash-scripts/wp-commands.php
+++ bash-scripts/wp-commands.php
@@ -4,11 +4,8 @@ $connectstr_dbname = getenv('DATABASE_NAME');
 $connectstr_dbusername = getenv('DATABASE_USERNAME');
 $connectstr_dbpassword = getenv('DATABASE_PASSWORD');
 
-$config_cmd = 'wp config set DB_NAME $connectstr_dbname --allow-root ;
-wp config set DB_USER $connectstr_dbusername --allow-root ;
-wp config set DB_PASSWORD $connectstr_dbpassword --allow-root ;
-wp config set DB_HOST $connectstr_dbhost --allow-root ;';
+$config_cmd = 'wp config set DB_NAME' . $connectstr_dbname . '--allow-root ;'
 
 echo 'starting wp-config update';
-echo shell_exec($config_cmd);
+echo '<pre>' . shell_exec($config_cmd) . '</pre>';
 echo 'wp-config updated';
