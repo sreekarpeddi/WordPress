452da3620b69d53528a16e1c2dc85ea3cf45b58f
Tim Loden <tim.m.loden@gmail.com>
2019-08-21 14:53:29 -0700

    added messages to wp-commands

--- bash-scripts/wp-commands.php
+++ bash-scripts/wp-commands.php
@@ -4,9 +4,11 @@ $connectstr_dbname = getenv('DATABASE_NAME');
 $connectstr_dbusername = getenv('DATABASE_USERNAME');
 $connectstr_dbpassword = getenv('DATABASE_PASSWORD');
 
-$config_cmd = 'wp config set DB_NAME $connectstr_dbname ;
-wp config set DB_USER $connectstr_dbusername ;
-wp config set DB_PASSWORD $connectstr_dbpassword ;
-wp config set DB_HOST $connectstr_dbhost ;';
+$config_cmd = 'wp config set DB_NAME $connectstr_dbname --allow-root ;
+wp config set DB_USER $connectstr_dbusername --allow-root ;
+wp config set DB_PASSWORD $connectstr_dbpassword --allow-root ;
+wp config set DB_HOST $connectstr_dbhost --allow-root ;';
 
+echo 'starting wp-config update';
 echo shell_exec($config_cmd);
+echo 'wp-config updated';
