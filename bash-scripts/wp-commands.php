ee9125bc83ced65c7c51f90a71cf92f334210db8
Tim Loden <tim.m.loden@gmail.com>
2019-08-21 14:50:46 -0700

    updated wp-commands for db config

--- bash-scripts/wp-commands.php
+++ bash-scripts/wp-commands.php
@@ -1,3 +1,12 @@
 <?php
-$output = shell_exec("wp --info");
-echo "<pre>".$output."</pre>";
+$connectstr_dbhost = getenv('DATABASE_HOST');
+$connectstr_dbname = getenv('DATABASE_NAME');
+$connectstr_dbusername = getenv('DATABASE_USERNAME');
+$connectstr_dbpassword = getenv('DATABASE_PASSWORD');
+
+$config_cmd = 'wp config set DB_NAME $connectstr_dbname ;
+wp config set DB_USER $connectstr_dbusername ;
+wp config set DB_PASSWORD $connectstr_dbpassword ;
+wp config set DB_HOST $connectstr_dbhost ;';
+
+echo shell_exec($config_cmd);
