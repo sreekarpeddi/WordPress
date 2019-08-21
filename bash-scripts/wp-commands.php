7be4c5472c83ba42d554fd0dce8cc23452492ff9
Tim Loden <tim.m.loden@gmail.com>
2019-08-21 15:08:07 -0700

    more testing

--- bash-scripts/wp-commands.php
+++ bash-scripts/wp-commands.php
@@ -4,9 +4,11 @@ $connectstr_dbname = getenv('DATABASE_NAME');
 $connectstr_dbusername = getenv('DATABASE_USERNAME');
 $connectstr_dbpassword = getenv('DATABASE_PASSWORD');
 
+$connectstr_dbname = escapeshellarg($connectstr_dbname);
+
 //$config_cmd = 'wp config set DB_NAME' . $connectstr_dbname . '--allow-root ;'
 
 echo 'starting wp-config update';
-echo $connectstr_dbname;
+echo 'dbname: ' . $connectstr_dbname;
 echo '<pre>' . shell_exec("wp config set DB_NAME $connectstr_dbname --allow-root ;") . '</pre>';
 echo 'wp-config updated';
