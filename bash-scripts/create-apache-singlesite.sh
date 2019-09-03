eb7f12442726e834f2c413970f1fc9e8a9a7c902
Tim Loden <tim.m.loden@gmail.com>
2019-09-03 16:16:17 -0700

    added apache setup script

---
+++ bash-scripts/create-apache-singlesite.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#Go to web root
+echo "Going to site root" ;
+cd /home/site/wwwroot/ ;
+
+# Get the wp core
+wp core download --allow-root ;
+
+# Run wp-commands.php directly
+wp eval-file /usr/local/bin/devops/wp-commands.php ;
+
+# Create .htaccess file
+cat >.htaccess <<EOL
+# BEGIN WordPress
+
+RewriteEngine On
+RewriteBase /
+RewriteRule ^index\.php$ - [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . /index.php [L]
+
+# END WordPress
+
+RewriteCond %{HTTP:X-ARR-SSL} ^$
+RewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
+EOL
+
+echo ".htaccess created" ;
+
+# Add SSL force to wp-config
+
+sed -i '/table_prefix/a define("FORCE_SSL_ADMIN", true); if ( isset($_SERVER["HTTP_X_ARR_SSL\"]) ) $_SERVER["HTTPS"]="on";' wp-config.php
+
+echo "updated wp-config with Azure specific SSL lines" ;
+
+__odi="
+   ______      ______                     
+  / ____/___ _/ / __ \________  __________
+ / /   / __  / / /_/ / ___/ _ \/ ___/ ___/
+/ /___/ /_/ / / ____/ /  /  __(__  |__  ) 
+\____/\__,_/_/_/   /_/   \___/____/____/
+"
+echo "$__odi"
+echo "Install completed, you can now go to: $1" ;
