f90e8d293008e5c2b3c3d1d576be9406c40b1763
Tim Loden <tim.m.loden@gmail.com>
2019-09-25 13:17:13 -0700

    added redis commands to wp-commands

--- bash-scripts/wp-commands.php
+++ bash-scripts/wp-commands.php
@@ -1,13 +1,22 @@
 <?php
+
+// get db settings
 $connectstr_dbhost = getenv('DATABASE_HOST');
 $connectstr_dbname = getenv('DATABASE_NAME');
 $connectstr_dbusername = getenv('DATABASE_USERNAME');
 $connectstr_dbpassword = getenv('DATABASE_PASSWORD');
 
+// get wp user 
 $admin_user = getenv('ADMIN_USER');
 $admin_password = getenv('ADMIN_PASSWORD');
 $admin_email = getenv('ADMIN_EMAIL');
 
+//get redis settings
+$redis_port = getenv('WP_REDIS_PORT');
+$redis_host = getenv('WP_REDIS_HOST');
+$redis_password = getenv('WP_REDIS_PASSWORD');
+
+// get site url
 $site_url = getenv('SITE_URL');
 
 // create wp-config
@@ -25,6 +34,16 @@ echo shell_exec("wp theme install https://github.com/timloden/CAWeb-Standard/arc
 // Install Updraft Plus, 2FA, SEO Framework, WP-Optimize and ReSmush.it
 
 echo shell_exec("wp plugin install updraftplus two-factor autodescription wp-optimize resmushit-image-optimizer wp-mail-smtp --activate --allow-root ;");
+
+if ($redis_host ) {
+    // get the redis plugin
+    echo shell_exec("wp plugin install redis-cache --activate --allow-root ;");
+    // set redis variables in wp-config
+    echo shell_exec("wp config set WP_REDIS_PORT '$redis_port' --allow-root ;");
+    echo shell_exec("wp config set WP_REDIS_HOST '$redis_host' --allow-root ;");
+    echo shell_exec("wp config set WP_REDIS_PASSWORD '$redis_password' --allow-root ;");
+    echo shell_exec("wp config set WP_REDIS_SELECTIVE_FLUSH true --raw --allow-root ;");
+}
 
 // Update plugins just in case
 
