95d48e7c2dcbc7d53536fa987d3d3848b75f8ca9
speddi <sreekar.peddi@state.ca.gov>
2019-12-19 12:13:32 -0800

    removed ssl line

--- bash-scripts/wp-commands.php
+++ bash-scripts/wp-commands.php
@@ -36,7 +36,6 @@ $divi_token = getenv('DIVI_TOKEN');
 // create wp-config
 
 echo shell_exec("wp config create --dbname='$connectstr_dbname' --dbuser='$connectstr_dbusername' --dbpass='$connectstr_dbpassword' --dbhost='$connectstr_dbhost' --allow-root ;");
-echo shell_exec("sed -i '/table_prefix/a define("FORCE_SSL_ADMIN", true); if ( isset($_SERVER["HTTP_X_ARR_SSL\"]) ) $_SERVER["HTTPS"]="on";' wp-config.php");
 echo ("updated wp-config with Azure specific SSL lines.....................");
 // install WP 
 
